2c272affe0062e1d8caddac8c38468dbb51b26ad
whria78 <whria78@gmail.com>
2025-03-02 12:59:03 +0900

    1.1.0.400

--- batch files/build setup.bat
+++
@@ -1,4 +0,0 @@
-"c:\Program Files\CreateInstall v4\ci" e:\svn\branches\medicalphoto.ci
-copy "E:\SVN\branches\release.txt" "\\10.0.0.2\www\medicalphoto\repo\release\beta\release.txt"
-copy "E:\SVN\branches\version.dat" "\\10.0.0.2\www\medicalphoto\repo\release\beta\version.dat"
-\\10.0.0.2\repo\release\beta\setup.exe

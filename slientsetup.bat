98aa49e76ab2f0a9bceb0750fae9629e9d9bd4c1
whria78 <whria78@gmail.com>
2025-03-02 13:49:11 +0900

    1.1.0.400

--- slientsetup.bat
+++
@@ -1,4 +0,0 @@
-"c:\Program Files (x86)\CreateInstall v4\ci" e:\svn\branches\slient.ci
-copy "\\i5\http\medicalphoto\repo\update2\setup.exe" "\\i5\http\medicalphoto\repo\release\old\slientsetup.exe"
-copy "E:\SVN\branches\release\silent.exe" "\\i5\http\medicalphoto\repo\update2\setup.exe"
-copy "E:\SVN\branches\update.txt" "\\i5\http\medicalphoto\repo\update2\update.txt"
--- test.bat
+++
@@ -1,4 +0,0 @@
-copy "E:\SVN\branches\release.txt" "\\asus\http\medicalphoto\repo\release\beta\release.txt"
-copy "E:\SVN\branches\version.dat" "\\asus\http\medicalphoto\repo\release\beta\version.dat"
-copy "E:\SVN\branches\release\setup.exe" "\\asus\http\medicalphoto\repo\release\beta\setup.exe"
-copy "E:\SVN\branches\release\setup_kor.exe" "\\asus\http\medicalphoto\repo\release\beta\setup_kor.exe"
